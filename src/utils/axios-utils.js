ca114647867c61deb1b5daca799770b8b0c8b129
markothedev <markothedevmail@gmail.com>
2023-04-17 07:22:29 +0100

    added correct baseurl to the admin dashboard

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -5,10 +5,8 @@ export const adminDetails = JSON.parse( localStorage.getItem("admin-user"))?.sta
 const TOKEN = adminDetails?.token
 
 const SHORT_NAME = JSON.parse(localStorage.getItem("shortName"))
-// export const URLnAME = 'rel8backend-production.up.railway.app'
-// const BASE_URL = `https://rel8backend-production.up.railway.app/tenant/${SHORT_NAME}/`
-export const URLnAME =  'localhost:8000'
-const BASE_URL = `http://localhost:8000/tenant/${SHORT_NAME}/`
+export const URLnAME = 'rel8backend-production.up.railway.app'
+const BASE_URL = `https://rel8backend-production.up.railway.app/tenant/${SHORT_NAME}/`
 
 export const privateRequest = axios.create({
     baseURL: BASE_URL,
