2bbe8e1afc348b2c6bb070300ce1ab8308a78dd5
markothedev <markothedevmail@gmail.com>
2023-08-21 11:24:06 +0100

    fixed the base url

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -6,14 +6,12 @@ export const adminDetails = JSON.parse(localStorage.getItem("admin-user"))
 const TOKEN = adminDetails?.token;
 
 const SHORT_NAME = JSON.parse(localStorage.getItem("shortName"));
-// export const URLnAME = 'rel8backend-production.up.railway.app'
-// const BASE_URL = `https://rel8backend-production.up.railway.app/tenant/${SHORT_NAME}/`
 
-export const URLnAME = "localhost:8000";
-// export const URLnAME = "rel8-corporate-backend-production.up.railway.app";
+// export const URLnAME = "localhost:8000";
+// const BASE_URL = `http://localhost:8000/tenant/${SHORT_NAME}/`
 
-const BASE_URL = `http://localhost:8000/tenant/${SHORT_NAME}/`
-// const BASE_URL = `https://rel8-corporate-backend-production.up.railway.app/tenant/${SHORT_NAME}/`;
+export const URLnAME = "rel8-corporate-backend-production.up.railway.app";
+const BASE_URL = `https://rel8-corporate-backend-production.up.railway.app/tenant/${SHORT_NAME}/`;
 
 export const privateRequest = axios.create({
   baseURL: BASE_URL,
